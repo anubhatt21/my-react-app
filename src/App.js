1695fc6f587da74f10a4ac4f958c395675cadb7f
Anuradha Bhatt <anudave21@gmail.com>
2017-11-01 19:00:02 -0500

    SetState

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      introText: 'I came from a state!'
+    }
+  }
+
   render() {
-    const introText = "Learning React"
     const headerText = "I am a title from a prop!"
 
     return (
       <div className="App">
-      <Header title={headerText} onClick={() => { alert('Hello')}}/>
+      <Header title={headerText} onClick={this.onClick.bind(this)}/>
         <p className="App-intro">
-        {introText}
+        {this.state.introText}
         </p>
       </div>
     );
   }
+  onClick() {
+    this.setState ({ introText: 'I clicked the button!'} )
+}
 }
 
 export default App;
@@ -27,7 +37,7 @@ class Header extends Component {
       <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">{this.props.title}</h1>
-          <button onClick={this.props.onClick}>Click Me! </button>
+          <button onClick={this.props.onClick}>Click Me!</button>
       </header>
         );
   }
